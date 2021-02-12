2b2b49dca158b70a919e5a9bdde258d9060ac563
Corey <corey.jbarr@gmail.com>
2021-02-12 11:10:30 -0600

    js recipe logic working

--- script/script.js
+++ script/script.js
@@ -1,7 +1,17 @@
-console.log('hwllo world');
-
+let defaultRecipe = [3, 1.5, .5, 1.5]
 
 
 function menu(value) {
     window.location.assign(`${value}.html`);
+}
+
+function recipe(value){
+    let newRecipe=[]
+    for(i=0;i<defaultRecipe.length;i++){
+        newRecipe.push(value*defaultRecipe[i])
+    }
+    for(i=0;i<newRecipe.length;i++){
+        document.getElementById([i]).innerHTML = newRecipe[i];
+    }
+
 }
