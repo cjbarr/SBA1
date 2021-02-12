1c298fa56dd2357ceeba9ee4a10f7647f6089172
Corey <corey.jbarr@gmail.com>
2021-02-12 13:26:31 -0600

    Regex done on recipe

--- script/script.js
+++ script/script.js
@@ -1,19 +1,29 @@
 let defaultRecipe = [3, 1.5, .5, 1.5]
 
+const recipeRegex = /^\d$/;
+
+function logIn(){
+    validateEmail(document.getElementById("logUser").value)
+}
+
 
 function menu(value) {
     window.location.assign(`${value}.html`);
 }
 
-function recipe(value){
+function recipe(loaves){
+    if (recipeRegex.test(loaves)){
     let newRecipe=[]
-  {
+  
     for(i=0;i<defaultRecipe.length;i++){
-        newRecipe.push(value*defaultRecipe[i])
+        newRecipe.push(loaves*defaultRecipe[i])
     }
     for(i=0;i<newRecipe.length;i++){
         document.getElementById([i]).innerHTML = newRecipe[i];
     }
-}
+
 
 }
+else{alert("Enter a valid number!")}
+}
+
